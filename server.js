79e6480d926e46667aa40276cb8ce9bffea585af
pyrojet <bakoora88@gmail.com>
2019-11-02 20:01:35 +0200

    enable cors

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ var express = require('express');
 var bodyparser = require('body-parser');
 var cheerio = require('cheerio')
 var app = express();
+var cors = require('cors');
 const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.listen(port, () => {
     console.log('Listening on port ' + port);
